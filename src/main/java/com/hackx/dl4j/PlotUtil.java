54935de30a9fc4c232358107d59648ffee835c44
曹磊 <caolei@mobike.com>
2017-07-09 11:18:32 +0800

    fix bug

--- src/main/java/com/hackx/dl4j/PlotUtil.java
+++ src/main/java/com/hackx/dl4j/PlotUtil.java
@@ -24,6 +24,7 @@ import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.api.ops.impl.indexaccum.IMax;
 import org.nd4j.linalg.factory.Nd4j;
 
+
 import javax.swing.*;
 import java.awt.*;
 
